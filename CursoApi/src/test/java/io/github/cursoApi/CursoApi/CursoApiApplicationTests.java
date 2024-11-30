f844a016c3804a1584a5ee896a1556e60e38ff5f
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-11-30 13:04:47 +0000

    first class

---
+++ CursoApi/src/test/java/io/github/cursoApi/CursoApi/CursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursoApi.CursoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
