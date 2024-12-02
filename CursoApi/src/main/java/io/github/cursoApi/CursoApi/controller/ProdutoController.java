8ce489a88e07b93ec1a33fc251a7894aec5e6260
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-12-02 15:32:51 +0000

    class 15

--- CursoApi/src/main/java/io/github/cursoApi/CursoApi/controller/ProdutoController.java
+++ CursoApi/src/main/java/io/github/cursoApi/CursoApi/controller/ProdutoController.java
@@ -1,18 +1,30 @@
 package io.github.cursoApi.CursoApi.controller;
 
 import io.github.cursoApi.CursoApi.model.Produto;
+import io.github.cursoApi.CursoApi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+
+        String id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
 
     }
