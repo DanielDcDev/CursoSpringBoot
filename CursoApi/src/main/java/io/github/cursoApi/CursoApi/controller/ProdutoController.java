a7227abf87e4992e6b584e988ccc5da06fb94d27
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-12-03 19:59:26 +0000

    first module complete

--- CursoApi/src/main/java/io/github/cursoApi/CursoApi/controller/ProdutoController.java
+++ CursoApi/src/main/java/io/github/cursoApi/CursoApi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package io.github.cursoApi.CursoApi.controller;
 
 import io.github.cursoApi.CursoApi.model.Produto;
 import io.github.cursoApi.CursoApi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,6 +26,23 @@ public class ProdutoController {
         return produto;
 
     }
-
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("{id}") String id){
+        produtoRepository.deleteById(id);
+    }
+    @PutMapping("id")
+    public void atualizar(@PathVariable("id") String id ,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome")String nome){
+        return produtoRepository.findByNome(nome);
+    }
 
 }
