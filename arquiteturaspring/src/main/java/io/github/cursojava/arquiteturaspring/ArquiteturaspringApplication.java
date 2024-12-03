3e67698d7f0bcac2fdaeb4b92f5ca7b17521eb9f
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-12-03 22:09:51 +0000

    second module

---
+++ arquiteturaspring/src/main/java/io/github/cursojava/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,14 @@
+package io.github.cursojava.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	}
+
+}
