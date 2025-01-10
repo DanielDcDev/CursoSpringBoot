07a4f16aa18b52f7053e470be2e4caf71623bea8
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2025-01-10 22:00:35 +0100

    aula 48

---
+++ LibraryApi/src/test/java/com/springdatajpa/LibraryApi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdatajpa.LibraryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
