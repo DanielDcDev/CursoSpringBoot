e099b3b9588823fac228160e2be4875bea816d33
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2025-01-14 03:50:52 +0100

    aula 53

---
+++ LibraryApi/src/main/java/com/LibraryApi/Config/DatabaseConfiguration.java
@@ -0,0 +1,59 @@
+//configuracao opcional, bom para saber mas tem outras formas de fazer e nao e tao necessario a um baixo nivel de codigo
+//para questoes de empresa e perfeito.
+
+
+
+package com.LibraryApi.Config;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.DriverManager;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    @Bean
+    public DataSource hikariDataSource(){
+
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10); //maximo de conexoes liberadas
+        config.setMinimumIdle(1); //tamanho inicial da pool
+        config.setPoolName("library");
+        config.setMaxLifetime(600000);//600 mil ms (10 minutos)
+        config.setConnectionTimeout(100000);//timout para conseguir uma conexao
+        config.setConnectionTestQuery("select 1 ");//query de teste no sql. Coisa basica
+
+        return new HikariDataSource(config);
+    }
+}
--- LibraryApi/src/test/java/com/LibraryApi/ApplicationTests.java
+++ LibraryApi/src/test/java/com/LibraryApi/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springdatajpa.LibraryApi;
+package com.LibraryApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
